3111bedf8e38e091a9b3450876060d471d8ac484
v.ivanova <viktoriia.ivanova@vista.com>
2024-08-21 22:49:19 +0300

    getAllContacts, updated generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,12 +1,12 @@
-import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { getAllContacts } from './getAllContacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
-    const data = await fs.readFile(PATH_DB, 'UTF-8');
-    const contacts = JSON.parse(data);
-    const newContacts =  new Array(number).fill(0).map(createFakeContact);
-   fs.writeFile(PATH_DB, JSON.stringify([...contacts,...newContacts], null, 2));
+  const contacts = await getAllContacts();
+  const newContacts = new Array(number).fill(0).map(createFakeContact);
+  fs.writeFile(PATH_DB, JSON.stringify([...contacts, ...newContacts], null, 2));
 };
 
 console.log(generateContacts(5));
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,10 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  const data = await fs.readFile(PATH_DB, 'UTF-8');
+  const contacts = JSON.parse(data);
+  return contacts;
+};
 
 console.log(await getAllContacts());
