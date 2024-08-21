1d97e0227ba0aeed5995100f0e87c9fa47516959
v.ivanova <viktoriia.ivanova@vista.com>
2024-08-21 23:04:36 +0300

    addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { getAllContacts } from './getAllContacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+    const contacts = await getAllContacts();
+    const newContact = createFakeContact();
+    fs.writeFile(PATH_DB, JSON.stringify([...contacts, newContact], null, 2));
+};
 
 addOneContact();
